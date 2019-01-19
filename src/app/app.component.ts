ce9a4d976ebe8b09611c2a2550e47c583ec2a58e
coturiv <coturiv@gmail.com>
2019-01-19 13:49:39 +0800

    Init proj with Ionic 4-rc.1(Angular 7.14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,39 @@
-import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from '@angular/core';
 
-import { VelosumLoginPage } from '../pages/velosum-login/velosum-login';
-import { ReferencePage } from '../pages/reference/reference';
-import { AboutVCiteMobilePage } from '../pages/about-vcite-mobile/about-vcite-mobile';
-import { SettingsPage } from '../pages/settings/settings';
-import { QrscanPage } from '../pages/qrscan/qrscan';
-import { CitationsPage } from '../pages/citations/citations';
-
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  templateUrl: 'app.html'
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
 })
-export class MyApp {
-  @ViewChild(Nav) navCtrl: Nav;
-    rootPage:any = CitationsPage;
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    }
+  ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // The platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
   }
-  goToVelosumLogin(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(VelosumLoginPage);
-  }goToReference(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(ReferencePage);
-  }goToCitations(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(CitationsPage);
-  }goToQrScan(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(QrscanPage);
-  }goToAboutVCiteMobile(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(AboutVCiteMobilePage);
-  }goToSettings(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(SettingsPage);
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,74 +1,27 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { VehicleInformationPage } from '../pages/vehicle-information/vehicle-information';
-import { ViolationDetailsPage } from '../pages/violation-details/violation-details';
-import { PhotoEvidencePage } from '../pages/photo-evidence/photo-evidence';
-import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
-import { ReviewFinishPage } from '../pages/review-finish/review-finish';
-import { VelosumLoginPage } from '../pages/velosum-login/velosum-login';
-import { AboutVCiteMobilePage } from '../pages/about-vcite-mobile/about-vcite-mobile';
-import { SettingsPage } from '../pages/settings/settings';
-import { ReferencePage } from '../pages/reference/reference';
-import { QrscanPage } from '../pages/qrscan/qrscan';
-import { CitationsPage } from '../pages/citations/citations';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { SQLite } from '@ionic-native/sqlite';
-import { Toast } from '@ionic-native/toast';
-import { Camera } from '@ionic-native/camera';
-import { AppVersion } from '@ionic-native/app-version';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { DataServiceProvider } from '../providers/data-service/data-service';
-import { HttpModule } from '@angular/http';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    VehicleInformationPage,
-    ViolationDetailsPage,
-    PhotoEvidencePage,
-    TabsControllerPage,
-    ReviewFinishPage,
-    VelosumLoginPage,
-    AboutVCiteMobilePage,
-    SettingsPage,
-    ReferencePage,
-    QrscanPage,
-    CitationsPage
-  ],
+  declarations: [AppComponent],
+  entryComponents: [],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    HttpModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    VehicleInformationPage,
-    ViolationDetailsPage,
-    PhotoEvidencePage,
-    TabsControllerPage,
-    ReviewFinishPage,
-    VelosumLoginPage,
-    AboutVCiteMobilePage,
-    SettingsPage,
-    ReferencePage,
-    QrscanPage,
-    CitationsPage
+    IonicModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    BarcodeScanner,
-    Camera,
-    SQLite,
-    Toast,
-    AppVersion,
-    DataServiceProvider
-  ]
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

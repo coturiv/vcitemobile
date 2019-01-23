0613c0ace438e901734a6369928fd4f9c8857043
coturiv <coturiv@gmail.com>
2019-01-23 22:33:32 +0800

    Update About and Reference page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AboutPage {
+  version: string = '0.0.1';
+  today: Date     = new Date();
+  company: string = 'PenFormsUSA Inc.';
 
 }
